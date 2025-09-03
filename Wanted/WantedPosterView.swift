695af238b50042a1ef491dbfb0d03e95b179e039
Michael Jones <michael.jones90@me.com>
2025-09-03 21:56:42 +0100

    updates to the look of the main poster body.

--- Wanted/WantedPosterView.swift
+++ Wanted/WantedPosterView.swift
@@ -7,6 +7,13 @@
 
 import SwiftUI
 
+/// creating a custom colour via code.
+extension ShapeStyle where Self == Color {
+    static var darkBrown: Color {
+        Color(red: 0.3, green: 0.2, blue: 0.1)
+    }
+}
+
 struct WantedPosterView: View {
     var image: Image?
     var crime: String
@@ -15,24 +22,63 @@ struct WantedPosterView: View {
     var paperOpacity: Double
     
     var body: some View {
-        VStack(spacing: 0) {
-            Text("WANTED")
-            Text("DEAD OR ALIVE")
+        ZStack {
+            Color.white
             
-            image
+            VStack(spacing: 0) {
+                Text("WANTED")
+                    .font(.custom("Superclarendon", size: 60))
+                    .fontWeight(.black)
+                    /// spaces the letters out.
+                    .tracking(5)
+                    .foregroundStyle(.darkBrown)
+                    .padding(.bottom, -10)
+                
+                Text("DEAD OR ALIVE")
+                    .font(.custom("Copperplate", size: 24))
+                    .bold()
+                    .tracking(2)
+                    .foregroundStyle(.darkBrown)
+                
+                image
+                
+                Text("For \(crime)")
+                    .font(.custom("Copperplate", size: 22))
+                    .multilineTextAlignment(.center)
+                    .bold()
+                    .tracking(1)
+                    .foregroundStyle(.darkBrown)
+                
+                Spacer()
+                
+                Text("REWARD")
+                    .font(.custom("Copperplate", size: 30))
+                    .tracking(3)
+                    .bold()
+                    .foregroundStyle(.darkBrown)
+                Text(reward)
+                    .font(.custom("Copperplate", size: 44))
+                    .bold()
+                    .tracking(2)
+                    .foregroundStyle(.darkBrown)
+                
+                Spacer()
+
+                Text(contact)
+                    .font(.custom("Copperplate", size: 18))
+                    .multilineTextAlignment(.center)
+                    .tracking(3)
+                    .foregroundStyle(.darkBrown)
+                    .padding(.bottom, 10)
+            }
+            /// provides a white shadow effect which helps pick the letters off the page.
+            .shadow(color: Color.white, radius: 3)
             
-            Text("For \(crime)")
-            
-            Spacer()
-            
-            Text("REWARD")
-            Text(reward)
-            
-            Spacer()
-            
-            Text("CONTACT")
-            Text(contact)
+            /// some horizontal padding.
+            .padding(.horizontal)
         }
+        /// this frame matches exactly with A4 paper.
+        .frame(width: 420, height: 594)
     }
 }
 
